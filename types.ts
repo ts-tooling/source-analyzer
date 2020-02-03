52efa318f3370bd946e9a37e6802b5d7a4049c03
Daniel Way <projects@danieldway.com>
2020-02-02 20:14:43 -0600

    Added API type members

--- types.ts
+++ types.ts
@@ -1,24 +1,35 @@
 /**
  * Describes class members included in the API.
  */
-export interface ApiClass {}
+export interface ApiClass {
+    /** Exposed API methods. */
+    methods: ApiMethod[];
+}
 
 /**
  * Describes method properties included in the API.
  */
-export interface ApiMethod {}
+export interface ApiMethod {
+    /** Each item represents a parameter name and type. Format "name: type" */
+    parameters: string[];
+    /** Return type, trimmed. */
+    returnType: string;
+}
 
 /**
  * Describes some exposed API module, to be used for analysis.
  */
-export interface ApiModule {}
+export interface ApiModule {
+    /** Unique module name. TODO namespacing */
+    name: string;
+}
 
 /**
  * A class-type exposed API module.
  */
-export interface ApiClassModule extends ApiModule {}
+export interface ApiClassModule extends ApiModule, ApiClass {}
 
 /**
  * A function-type exposed API module.
  */
-export interface ApiFunctionModule extends ApiModule {}
+export interface ApiFunctionModule extends ApiModule, ApiMethod {}
