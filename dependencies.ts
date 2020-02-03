ff23eb281701ba74ff89db109bb3a4867a306086
Daniel Way <projects@danieldway.com>
2020-02-02 22:22:58 -0600

    Start from the demo app

--- dependencies.ts
+++ dependencies.ts
@@ -1,6 +1,21 @@
 import {ApiDependencyDiGraph} from "./types";
+import * as ts from "typescript";
+import * as path from "path";
+import {locateFileModules} from "./traversal";
 
 export function getApplicationDependencyGraph(file: string): ApiDependencyDiGraph {
+    // Setup TypeScript AST
+    const program = ts.createProgram(['sample-source-1/sample-source.ts'], {});
+    const files = program.getSourceFiles().filter((file) => file.fileName.indexOf('sample-source-') > -1);
+
+    // Scan the source files for exposed API modules
+    files.forEach((file) => {
+        console.group(`Searching for modules in ${path.basename(file.fileName)}...`);
+        const apiModules = locateFileModules(file);
+        console.log(`Located ${apiModules.length} modules: ${JSON.stringify(apiModules)}`);
+        console.groupEnd();
+    });
+
     // TODO graph stuff
     return null;
 }
