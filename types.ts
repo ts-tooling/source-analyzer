0259e38c4b2be4bf7c9bcdfe6a264a497c63eb34
Daniel Way <projects@danieldway.com>
2020-02-02 20:09:36 -0600

    Added documentation to API types

--- types.ts
+++ types.ts
@@ -1,3 +1,24 @@
-export interface Module {}
-export interface ClassModule extends Module {}
-export interface FunctionModule extends Module {}
+/**
+ * Describes class members included in the API.
+ */
+export interface ApiClass {}
+
+/**
+ * Describes method properties included in the API.
+ */
+export interface ApiMethod {}
+
+/**
+ * Describes some exposed API module, to be used for analysis.
+ */
+export interface ApiModule {}
+
+/**
+ * A class-type exposed API module.
+ */
+export interface ApiClassModule extends ApiModule {}
+
+/**
+ * A function-type exposed API module.
+ */
+export interface ApiFunctionModule extends ApiModule {}
