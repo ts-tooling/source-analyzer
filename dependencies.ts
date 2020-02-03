0c1e5cf59abf1c0555414221aa115384752619b8
Daniel Way <projects@danieldway.com>
2020-02-02 22:35:10 -0600

    Added dependency cleaning

--- dependencies.ts
+++ dependencies.ts
@@ -15,12 +15,27 @@ export function getApplicationDependencyGraph(filename: string): ApiDependencyDi
     const apiModules: ApiModule[] = [];
     files.forEach((file) => apiModules.push(...locateFileModules(file)));
 
+    // Clean the dependencies
+    cleanModuleDependencies(apiModules);
+
     console.log(JSON.stringify(apiModules));
 
     // TODO graph stuff
     return null;
 }
 
+// Note: this is a workaround until I can better-distinguish identifiers from dependencies
+function cleanModuleDependencies(apiModules: ApiModule[]) {
+    // Accum. legitimate exposed API module names
+    const legitModules: string[] = apiModules.map((apiModule) => apiModule.name);
+    // Clean all modules' dependencies of invalid identifiers (including themselves)
+    apiModules.forEach((apiModule) => apiModule.dependencies =
+        apiModule.dependencies.filter((dep) =>
+            dep !== apiModule.name &&
+            legitModules.indexOf(dep) > -1));
+}
+
+// given a/b/c/d.ts, will return c; for filtering to immediate source files
 function getImmediateDirectoryName(file: string): string {
     const pathPaths = file.split('/');
     return pathPaths[pathPaths.length - 2];
