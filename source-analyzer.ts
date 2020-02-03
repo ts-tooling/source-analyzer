891e97e2c4bb6d4ea55f4e08db522f206263f1fc
Daniel Way <projects@danieldway.com>
2020-02-02 22:29:29 -0600

    Updated package exports

--- source-analyzer.ts
+++ source-analyzer.ts
@@ -1,2 +1,17 @@
-export { Module, ClassModule, FunctionModule } from './types';
-export { recursiveNodeSearch, recursiveFileSearch, locateFileModules } from './traversal';
+export {
+    ApiModule,
+    ApiClassModule,
+    ApiFunctionModule,
+    ApiDependencyDiGraph,
+    ApiDependencyEdge
+} from './types';
+
+export {
+    recursiveNodeSearch,
+    recursiveFileSearch,
+    locateFileModules
+} from './traversal';
+
+export {
+    getApplicationDependencyGraph
+}  from './dependencies';
