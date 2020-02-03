fb2e9a8155d88a1764a0390e7931b3f82649fa53
Daniel Way <projects@danieldway.com>
2020-02-02 22:01:24 -0600

    Added quick demo app

--- demo.ts
+++ demo.ts
@@ -1,6 +1,15 @@
-import { ApiModule } from './types';
+import * as ts from 'typescript';
+import * as path from "path";
+import { locateFileModules } from "./traversal";
 
-const test: ApiModule = {
-    name: 'HelloWorld'
-};
-// TODO something useful
+// Setup TypeScript AST
+const program = ts.createProgram(['sample-source-1/sample-source.ts'], {});
+const files = program.getSourceFiles().filter((file) => file.fileName.indexOf('sample-source-') > -1);
+
+// Scan the source files for exposed API modules
+files.forEach((file) => {
+   console.group(`Searching for modules in ${path.basename(file.fileName)}...`);
+   const apiModules = locateFileModules(file);
+   console.log(`Located ${apiModules.length} modules: ${JSON.stringify(apiModules)}`);
+   console.groupEnd();
+});
