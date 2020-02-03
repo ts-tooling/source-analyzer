34a9af1ac24e23948c62d5ff001c92de1741bddd
Daniel Way <projects@danieldway.com>
2020-02-02 20:19:12 -0600

    Implemented recursiveNodeSearch with docs

--- traversal.ts
+++ traversal.ts
@@ -1,10 +1,24 @@
 import * as ts from 'typescript';
 import { ApiModule } from "./types";
 
+/**
+ * Recursively searches AST for nodes truthy with the given matcher.
+ * @param sourceFile The TS file to search. Note: This is a huge stability/performance consideration for TS API.
+ * @param root The node to begin searching from.
+ * @param matcher The lambda to determine whether a given node is a match or not.
+ */
 export function recursiveNodeSearch(sourceFile: ts.SourceFile, root: ts.Node,
                                     matcher: (node: ts.Node) => boolean): ts.Node[] {
-    // TODO
-    return null;
+    let matches: ts.Node[] = [];
+
+    if (matcher(root))
+        matches.push(root);
+
+    const children = root.getChildren(sourceFile);
+    if (children && children.length > 0)
+        children.forEach((child) => matches.push(...recursiveNodeSearch(sourceFile, child, matcher)));
+
+    return matches;
 }
 
 export function recursiveFileSearch(sourceFile: ts.SourceFile,
