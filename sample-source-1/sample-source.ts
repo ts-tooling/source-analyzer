4d9ebff06f05fe6e2c6d8f1ef4ce1e10f0b5ac34
Daniel Way <projects@danieldway.com>
2020-02-02 22:10:18 -0600

    Updated sample source

--- sample-source-1/sample-source.ts
+++ sample-source-1/sample-source.ts
@@ -4,7 +4,14 @@ import { SomeClassA } from './some-class-a';
 import { SomeClassB } from "./some-class-b";
 import { TestFunction1, TestFunction2 } from "./some-exports-1";
 
-new SomeClassA();
-new SomeClassB();
-TestFunction1();
-TestFunction2('', 1);
+export class SampleSource {
+    constructor(
+        a: SomeClassA,
+        b: SomeClassB
+    ) {}
+
+    test() {
+        TestFunction1();
+        TestFunction2('', 1);
+    }
+}
